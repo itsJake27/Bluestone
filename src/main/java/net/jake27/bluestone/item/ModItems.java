6d774b6aac514a2c430f7b3e5a2005f986018a22
itsJake27 <58084434+itsJake27@users.noreply.github.com>
2024-12-29 18:30:56 +0000

    New

---
+++ src/main/java/net/jake27/bluestone/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.jake27.bluestone.item;
+
+import net.jake27.bluestone.BluestoneMOD;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, BluestoneMOD.MOD_ID);
+
+    public static final RegistryObject<Item> Bluestone = ITEMS.register("bluestone",
+        () -> new Item(new Item.Properties()));
+
+    public static void register (IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
+
