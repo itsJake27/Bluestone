0dd63328a392c087f596ea3e9766abcae4ad558a
itsJake27 <58084434+itsJake27@users.noreply.github.com>
2024-12-30 16:43:14 +0000

    New

--- src/main/java/net/jake27/bluestone/block/ModBlocks.java
+++ src/main/java/net/jake27/bluestone/block/ModBlocks.java
@@ -19,14 +19,14 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, BluestoneMOD.MOD_ID);
 
-    public static final RegistryObject<Block> BLUESTONE_BLOCKORE = registerBlock(
+    public static final RegistryObject<Block> BLUESTONE_BLOCKORE = registerBlock("bluestone_blockore",
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
 
-    private static <T extends Block> RegistryObject <T> registerBlock(Supplier<T> block) {
-        RegistryObject<T> toReturn = BLOCKS.register("bluestone_blockore", block);
-        RegisterBlockItem("bluestone_blockore", toReturn);
+    private static <T extends Block> RegistryObject <T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        RegisterBlockItem(name, toReturn);
         return toReturn;
     }
 
